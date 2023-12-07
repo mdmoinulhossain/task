ad12005bfbff523371ffc329f36220eed5730478
MD. MOINUL Hossian <mdmoinulhossain100@gmail.com>
2023-12-07 15:23:11 +0600

    design uploaded

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to open the popup
+function openPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
