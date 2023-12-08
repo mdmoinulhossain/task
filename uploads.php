feee91a3eda2e26a9d57d6f51d47b172a29d080a
MD. MOINUL Hossian <mdmoinulhossain100@gmail.com>
2023-12-08 12:25:32 +0600

    uploaded file by php

---
+++ uploads.php
@@ -0,0 +1,24 @@
+<?php
+// include database file
+include './db-conn.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_FILES['file']) {
+        $file_name = $_FILES['file']['name'];
+        $file_tmp = $_FILES['file']['tmp_name'];
+
+        // Move the uploaded file to a directory (adjust the path as needed)
+        $upload_path = './assets/uploads/';
+        move_uploaded_file($file_tmp, $upload_path . $file_name);
+
+        // Store file information in the database
+        $sql = "INSERT INTO `files` (`filename`, `file_path`) VALUES ('$file_name', '$upload_path')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "File uploaded successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+}
+
+$conn->close();
